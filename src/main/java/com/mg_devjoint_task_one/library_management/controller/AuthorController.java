de10d2c21c274bc9f24c94dacce512a0baee2de8
Mahabbat Gozalov <mgzlovcontact@gmail.com>
2026-07-21 04:39:22 +0400

    feat: implement generic pagination support for author listing

--- src/main/java/com/mg_devjoint_task_one/library_management/controller/AuthorController.java
+++ src/main/java/com/mg_devjoint_task_one/library_management/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.mg_devjoint_task_one.library_management.controller;
 import com.mg_devjoint_task_one.library_management.dto.request.create.CreateAuthorRequest;
 import com.mg_devjoint_task_one.library_management.dto.request.update.UpdateAuthorRequest;
 import com.mg_devjoint_task_one.library_management.dto.response.AuthorResponse;
+import com.mg_devjoint_task_one.library_management.dto.response.PageResponse;
 import com.mg_devjoint_task_one.library_management.service.AuthorService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/api/v1/authors")
@@ -26,17 +30,26 @@ public class AuthorController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PutMapping("/{authorId}")
-    public ResponseEntity<AuthorResponse> updateAuthor(
-            @PathVariable UUID authorId,
-            @Valid @RequestBody UpdateAuthorRequest request) {
-        AuthorResponse response = authorService.updateAuthor(authorId, request);
-        return ResponseEntity.ok(response);
+    @GetMapping
+    ResponseEntity<PageResponse<AuthorResponse>> getAllAuthors(@RequestParam(defaultValue = "1") int page,
+                                                               @RequestParam(defaultValue = "10") int size
+    ) {
+        var body = authorService.getAllAuthors(page, size);
+
+        return ResponseEntity.ok(body);
     }
 
     @GetMapping("/{authorId}")
     public ResponseEntity<AuthorResponse> getAuthorById(@PathVariable UUID authorId) {
         AuthorResponse response = authorService.getAuthorById(authorId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{authorId}")
+    public ResponseEntity<AuthorResponse> updateAuthor(
+            @PathVariable UUID authorId,
+            @Valid @RequestBody UpdateAuthorRequest request) {
+        AuthorResponse response = authorService.updateAuthor(authorId, request);
         return ResponseEntity.ok(response);
     }
 
