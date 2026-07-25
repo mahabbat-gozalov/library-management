d48794b25b2682d62565b4f8b4668d958bfb9037
Mahabbat Gozalov <mgzlovcontact@gmail.com>
2026-07-25 22:43:51 +0400

    feat(auth): add service classes and interfaces

---
+++ src/main/java/com/mg_devjoint_task_one/library_management/service/MailService.java
@@ -0,0 +1,5 @@
+package com.mg_devjoint_task_one.library_management.service;
+
+public interface MailService {
+    boolean sendTemporaryPasswordEmail(String email, String sendTemporaryPasswordEmail);
+}
