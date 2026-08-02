54d45fa4bed25276139077a1e2b31c686853f2fe
Mahabbat Gozalov <mgzlovcontact@gmail.com>
2026-08-02 14:38:44 +0400

    feat: add getAllUsers method to user service

--- src/main/java/com/mg_devjoint/library_management/service/impl/UserServiceImpl.java
+++ src/main/java/com/mg_devjoint/library_management/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.mg_devjoint.library_management.service.impl;
 
-import com.mg_devjoint.library_management.dto.response.CreateUserResponse;
+import com.mg_devjoint.library_management.dto.response.*;
 import com.mg_devjoint.library_management.exception.DuplicateEmailException;
 import com.mg_devjoint.library_management.exception.NotFoundException;
+import com.mg_devjoint.library_management.mapper.UserMapper;
 import com.mg_devjoint.library_management.model.User;
 import com.mg_devjoint.library_management.repository.UserRepository;
 import com.mg_devjoint.library_management.service.UserService;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,19 +18,6 @@ public class UserServiceImpl implements UserService {
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    @Override
-    public User findUserByEmail(String email) {
-        return userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new NotFoundException("User not found"));
-    }
-
-    @Override
-    public User findUserById(UUID userId) {
-
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException("User not found"));
     }
 
     @Override
@@ -51,6 +40,29 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public PageResponse<UserResponse> getAllUsers(int page, int size) {
+        Pageable pageable = getPageable(page, size);
+        Page<User> userPage = userRepository.findAll(pageable);
+
+        Page<UserResponse> userResponsePage = userPage.map(UserMapper::toUserResponse);
+
+        return PageResponse.of(userResponsePage);
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    @Override
+    public User findUserById(UUID userId) {
+
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    @Override
     public void changePassword(User user, String encodedNewPassword) {
 
         user.setPassword(encodedNewPassword);
@@ -68,6 +80,14 @@ public class UserServiceImpl implements UserService {
 
     private boolean isEmailExist(String email) {
         return userRepository.existsByEmail(email);
+    }
+
+    private Pageable getPageable(int pageNumber, int pageSize) {
+
+        pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+        pageSize = pageSize <= 0 ? 10 : pageSize;
+
+        return PageRequest.of(pageNumber - 1, pageSize);
     }
 
 }
